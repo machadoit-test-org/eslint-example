da4e435f5fc087e154c34159aa10411c264348bb
Joao Machado <machado@codacy.com>
2021-02-11 11:33:34 +0000

    add more issues

---
+++ test6.js
@@ -0,0 +1,24 @@
+
+
+
+var potato = "";
+
+if (potato === "potato") {
+  console.log("hey you");
+}
+
+
+
+
+var foo = '' ;
+
+if(foo  === "bar") {
+  console.log( 'hey' )
+}
+
+
+var foo77 = '' ;
+
+if(foo77  === "baaaaaaaaaar") {
+  console.log( 'heaaaaaaay' )
+}
