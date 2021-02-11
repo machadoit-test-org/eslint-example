a1dfd647615d2c7df2c123f8abb98724084f8d90
Joao Machado <machado@codacy.com>
2021-02-11 11:08:43 +0000

    try to have strange suggestions

---
+++ test2.js
@@ -0,0 +1,19 @@
+var foo = '' ;
+
+if(foo  === "bar") {
+  console.log( 'hey' )
+}
+
+
+var foo88 = '' ;
+
+if(foo88  === "b88") {
+  console.log( 'he88' );
+}
+
+
+var foo77 = '' ;
+
+if(foo77  === "baaaaaaaaaar") {
+  console.log( 'heaaaaaaay' )
+}
